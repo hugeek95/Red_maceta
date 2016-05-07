6f1977039febc9a2bf0210a20fd7780adac5a3a9
MarioHdpz <mariohdpz@gmail.com>
2016-05-07 16:59:48 -0500

    Primer listado de productos
    La galería ya se conecta a la base de datos e imprime las fichas con
    esa información. Imprime todos.

--- galeria.php
+++ galeria.php
@@ -2,14 +2,14 @@
 <html>
 <body>
   <?php
+    header("Content-Type: text/html;charset=utf-8");
     $host_name  = "db624747361.db.1and1.com";
     $database   = "db624747361";
     $user_name  = "dbo624747361";
     $password   = "tomates";
 
-
-    $connect = mysqli_connect($host_name, $user_name, $password, $database);
-
+    $conn = mysqli_connect($host_name, $user_name, $password, $database);
+    $conn->query("SET NAMES 'utf8'");
     if(mysqli_connect_errno())
     {
     echo '<p>Error al conectar a base de datos: '.mysqli_connect_error().'</p>';
@@ -17,13 +17,11 @@
 
     $sql = "SELECT * FROM Producto";
     $result = mysqli_query($conn, $sql);
-    echo "string";
-    echo mysqli_num_rows($result);
+    echo '<div class="row">';
     while($row = mysqli_fetch_assoc($result)){
-        $x = 1;
-        if ($x == 1){
-        echo '<div class="row">';
-        }
+    $sql_productor = "SELECT * FROM productors WHERE id LIKE ".$row['IDProductor'];
+    $result_productor  = mysqli_query($conn, $sql_productor);
+    $row2 = mysqli_fetch_assoc($result_productor);
     ?>
       <!--  card <?php echo $x ?> -->
       <div class="col l4 m6 s12">
@@ -33,14 +31,14 @@
               </div>
               <div class="card-content">
                 <p class="card-title"><?php echo $row["Nombre"] ?></p>
-                <p class="card-subtitle">Texcoco - 42km</p>
+                <p class="card-subtitle"><?php echo $row2["lugar"]; ?></p>
                 <p class="precio">$<?php echo $row["Precio"] ?> MXN - <?php echo $row["Unidad"] ?></p>
               </div>
               <!-- Productor -->
               <a class="modal-trigger" href="#productor<?php echo $row["id"] ?>">
                 <div class="foto-productor">
-                  <img src="img/jpgs/vlad.png" class="circle responsive-img" alt="" />
-                  <p>Rancho Xaltipa</p>
+                  <img src="img/<?php echo $row2["foto_productor"]; ?>" class="circle responsive-img" alt="" />
+                  <p><?php echo $row2["alias"]; ?></p>
                 </div>
               </a>
               <!-- Productor Modal Structure -->
@@ -48,19 +46,19 @@
                     <div class="modal-content">
                       <div class="row">
                         <div class="col l6 m6 s12">
-                          <img src="img/jpgs/vlad.png" class="circle responsive-img" alt="" />
+                          <img src="img/<?php echo $row2["foto_productor"]; ?>" class="circle responsive-img" alt="" />
                         </div>
                         <div class="col l6 m6 s12 ">
-                            <h4>Rancho Xaltipa</h4>
-                            <h5>Texcoco</h5>
-                            <p><?php echo $row["Descripcion"] ?> </p>
+                            <h4><?php echo $row2["alias"]; ?></h4>
+                            <h5><?php echo $row2["lugar"]; ?></h5>
+                            <p><?php echo $row2["descripcion"] ?> </p>
                             <div class="row">
                             <div class="medallas">
                                 <!--<span class="left">Medallas:</span>-->
-                                <a class="tooltipped" data-position="top" data-delay="50" data-tooltip="Local"><img class="responsive-img" src="img/png/local.png" alt=""/></a>
-                                <a class="tooltipped" data-position="top" data-delay="50" data-tooltip="Orgánico"><img class="responsive-img" src="img/png/organico.png" alt=""/></a>
-                                <a class="tooltipped" data-position="top" data-delay="50" data-tooltip="Pequeño"><img class="responsive-img" src="img/png/pequeño.png" alt=""/></a>
-                                <a class="tooltipped" data-position="top" data-delay="50" data-tooltip="Urbano"><img class="responsive-img" src="img/png/urbano.png" alt=""/></a>
+                                <a class="tooltipped <?php if ($row2["local"] == 0) {echo "hide"; } ?>" data-position="top" data-delay="50" data-tooltip="Local"><img class="responsive-img" src="img/png/local.png" alt=""/></a>
+                                <a class="tooltipped <?php if ($row2["organico"] == 0) {echo "hide"; } ?>" data-position="top" data-delay="50" data-tooltip="Orgánico"><img class="responsive-img" src="img/png/organico.png" alt=""/></a>
+                                <a class="tooltipped <?php if ($row2["vulnerable"] == 0) {echo "hide"; } ?>" data-position="top" data-delay="50" data-tooltip="Vulnerable"><img class="responsive-img" src="img/png/pequeño.png" alt=""/></a>
+                                <a class="tooltipped <?php if ($row2["urbano"] == 0) {echo "hide"; } ?>" data-position="top" data-delay="50" data-tooltip="Urbano"><img class="responsive-img" src="img/png/urbano.png" alt=""/></a>
                             </div>
                             </div>
                             <a href="#!" class=" modal-action modal-close waves-effect waves-green btn green">¡Entendido!</a>
@@ -73,10 +71,10 @@
               <!-- /Productor -->
               <!-- Atrás card -->
               <div class="card-reveal">
-                <span class="card-title">LECHE ENTERA TRADICIONAL<i class="material-icons right">close</i></span>
-                <span class="precio_grande red-text left">$30 MXN</span>
+                <span class="card-title"><?php echo $row["Nombre"] ?><i class="material-icons right">close</i></span>
+                <span class="precio_grande red-text left">$<?php echo $row["Precio"] ?> MXN</span>
                 <br>
-                <p class="descripcion">Leche de vaca pasteurizada elaborada de la manera tradicional, 100% pura, sin rebajar. Al hervirla se consigue una exquisita nata.</p>
+                <p class="descripcion"><?php echo $row["Descripcion"] ?></p>
                 <div class="compra">
                         <a class="waves-effect waves-light btn red">A la canasta</a>
                 </div>
