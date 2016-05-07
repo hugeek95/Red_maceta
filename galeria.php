06d9ea57591660944a30131cbecd70d633774e5a
MarioHdpz <mariohdpz@gmail.com>
2016-05-07 13:57:48 -0500

    Impresión correcta de cards
    Ya se acomodan bien las fichas, estoy intentando conectar con la base de
    datos.

--- galeria.php
+++ galeria.php
@@ -1,31 +1,50 @@
 <!DOCTYPE html>
 <html>
 <body>
+  <?php
+    $host_name  = "db624747361.db.1and1.com";
+    $database   = "db624747361";
+    $user_name  = "dbo624747361";
+    $password   = "tomates";
 
-  <?php  for ($x = 1; $x <= 8; $x++){
-              if (fmod($x-1, 3) == 0){
-              echo '<div class="row">'; }
-  ?>
-      <!--  card 1 -->
+
+    $connect = mysqli_connect($host_name, $user_name, $password, $database);
+
+    if(mysqli_connect_errno())
+    {
+    echo '<p>Error al conectar a base de datos: '.mysqli_connect_error().'</p>';
+    }
+
+    $sql = "SELECT * FROM Producto";
+    $result = mysqli_query($conn, $sql);
+    echo "string";
+    echo mysqli_num_rows($result);
+    while($row = mysqli_fetch_assoc($result)){
+        $x = 1;
+        if ($x == 1){
+        echo '<div class="row">';
+        }
+    ?>
+      <!--  card <?php echo $x ?> -->
       <div class="col l4 m6 s12">
           <div class="card">
               <div class="card-image waves-effect waves-block waves-light">
-                <img class="activator" src="img/productos/leche.jpg">
+                <img class="activator" src="img/productos/<?php echo $row["Imagen"] ?>.jpg">
               </div>
               <div class="card-content">
-                <p class="card-title">LECHE ENTERA TRADICIONAL</p>
+                <p class="card-title"><?php echo $row["Nombre"] ?></p>
                 <p class="card-subtitle">Texcoco - 42km</p>
-                <p class="precio">$30 MXN - litro</p>
+                <p class="precio">$<?php echo $row["Precio"] ?> MXN - <?php echo $row["Unidad"] ?></p>
               </div>
               <!-- Productor -->
-              <a class="modal-trigger" href="#productor1">
+              <a class="modal-trigger" href="#productor<?php echo $row["id"] ?>">
                 <div class="foto-productor">
                   <img src="img/jpgs/vlad.png" class="circle responsive-img" alt="" />
                   <p>Rancho Xaltipa</p>
                 </div>
               </a>
               <!-- Productor Modal Structure -->
-              <div id="productor1" class="modal modal-productor">
+              <div id="productor<?php echo $row["id"] ?>" class="modal modal-productor">
                     <div class="modal-content">
                       <div class="row">
                         <div class="col l6 m6 s12">
@@ -34,7 +53,7 @@
                         <div class="col l6 m6 s12 ">
                             <h4>Rancho Xaltipa</h4>
                             <h5>Texcoco</h5>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+                            <p><?php echo $row["Descripcion"] ?> </p>
                             <div class="row">
                             <div class="medallas">
                                 <!--<span class="left">Medallas:</span>-->
@@ -70,10 +89,11 @@
               <!-- /Botón delantero -->
           </div>
           </div>
-      <!--  /card 1 -->
-      <?php  if (fmod($x, 3) == 0){
-                  echo '</div>'; }
+      <!--  /card <?php echo "$x" ?> -->
+      <?php
+        $x++;
         }
       ?>
+      </div>
 </body>
 </html>
