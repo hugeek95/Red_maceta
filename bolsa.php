e0bab566feca74248118a363b603660e3c06201f
MarioHdpz <mariohdpz@gmail.com>
2016-05-11 10:34:37 -0500

    Lista carrito - 1 x 1
    Intento de agregar productos a la lista

--- bolsa.php
+++ bolsa.php
@@ -14,80 +14,27 @@
     {
     echo '<p>Error al conectar a base de datos: '.mysqli_connect_error().'</p>';
     }
-
-    $sql = "SELECT * FROM Producto WHERE nombre LIKE '%$b%'";
+    $b = $_REQUEST["b"];
+    $sql = "SELECT * FROM Producto WHERE id LIKE '%$b%'";
     $result = mysqli_query($conn, $sql);
     while($row = mysqli_fetch_assoc($result)){
     ?>
-      <!--  card <?php echo $x ?> -->
-      <div class="col l4 m6 s12">
-          <div class="card">
-              <div class="card-image waves-effect waves-block waves-light">
-                <img class="activator" src="img/productos/<?php echo $row["Imagen"] ?>.jpg" onerror="this.src='img/productos/Default.jpg'">
-              </div>
-              <div class="card-content">
-                <p class="card-title activator"><?php echo $row["Nombre"] ?></p>
-                <p class="card-subtitle activator"><?php echo $row2["lugar"]; ?></p>
-                <p class="precio activator">$<?php echo $row["Precio"] ?> MXN - <?php echo $row["Unidad"] ?></p>
-              </div>
-              <!-- Productor -->
-              <a class="modal-trigger" href="#productor<?php echo $row["id"] ?>">
-                <div class="foto-productor">
-                  <img src="img/img_productors/<?php echo $row2["foto_productor"]; ?>.jpg" class="circle responsive-img" alt="" />
-                  <p><?php echo $row2["alias"]; ?></p>
-                </div>
-              </a>
-              <!-- Productor Modal Structure -->
-              <div id="productor<?php echo $row["id"] ?>" class="modal modal-productor">
-                    <div class="modal-content">
-                      <div class="row">
-                        <div class="col l6 m6 s12">
-                          <img src="img/img_productors/<?php echo $row2["foto_productor"]; ?>.jpg" class="circle responsive-img" alt="" />
-                        </div>
-                        <div class="col l6 m6 s12 ">
-                            <h4><?php echo $row2["alias"]; ?></h4>
-                            <h5><?php echo $row2["lugar"]; ?></h5>
-                            <p><?php echo $row2["descripcion"] ?> </p>
-                            <div class="row">
-                            <div class="medallas">
-                                <!--<span class="left">Medallas:</span>-->
-                                <a class="tooltipped <?php if ($row2["local"] == 0) {echo "hide"; } ?>" data-position="top" data-delay="50" data-tooltip="Local"><img class="responsive-img" src="img/png/local.png" alt=""/></a>
-                                <a class="tooltipped <?php if ($row2["organico"] == 0) {echo "hide"; } ?>" data-position="top" data-delay="50" data-tooltip="Orgánico"><img class="responsive-img" src="img/png/organico.png" alt=""/></a>
-                                <a class="tooltipped <?php if ($row2["vulnerable"] == 0) {echo "hide"; } ?>" data-position="top" data-delay="50" data-tooltip="Vulnerable"><img class="responsive-img" src="img/png/vulnerable.png" alt=""/></a>
-                                <a class="tooltipped <?php if ($row2["urbano"] == 0) {echo "hide"; } ?>" data-position="top" data-delay="50" data-tooltip="Urbano"><img class="responsive-img" src="img/png/urbano.png" alt=""/></a>
-                            </div>
-                            </div>
-                            <a href="#!" class=" modal-action modal-close waves-effect waves-green btn green">¡Entendido!</a>
-                        </div>
-                    </div>
-                </div>
-
-              </div>
-              <!-- /Productor Modal Structure -->
-              <!-- /Productor -->
-              <!-- Atrás card -->
-              <div class="card-reveal">
-                <span class="card-title"><?php echo $row["Nombre"] ?><i class="material-icons right">close</i></span>
-                <span class="precio_grande red-text left">$<?php echo $row["Precio"] ?> MXN</span>
-                <br>
-                <p class="descripcion"><?php echo $row["Descripcion"] ?></p>
-                <div class="compra">
-                        <a class="waves-effect waves-light btn red">A la canasta</a>
-                </div>
-              </div>
-              <!-- /Atrás card -->
-              <!-- Botón delantero -->
-              <div class="boton-delantero">
-                <a><img src="img/png/btn_canasta_compra.png" class="responsive-img waves-effect waves-light" alt="" /></a>
-              </div>
-              <!-- /Botón delantero -->
-          </div>
-          </div>
-      <!--  /card <?php echo "$x" ?> -->
-      <?php
-        $x++;
-        }
-      ?>
-      </div>
+      <!--  row  -->
+      <tr>
+        <td><img  src="img/productos/<?php echo $row["Imagen"] ?>.jpg" class="img-canasta"alt="" /></td>
+        <td><span><?php echo $row["Nombre"] ?></span></td>
+        <td>$<span id="precio"><?php echo $row["Precio"] ?></span> MXN <?php echo $row["Unidad"] ?></td>
+        <td>
+        <i class="controles material-icons green-text" onclick="document.getElementById('cantidad<?php echo $b ?>').stepDown()">remove_circle</i>
+        <input value="0" id="cantidad<?php echo $b ?>" type="number" min="0" max="20" class="validate">
+        <i class="controles material-icons green-text" onclick="document.getElementById('cantidad<?php echo $b ?>').stepUp()">add_circle</i>
+        </td>
+        <td id="importe">$0.00</td>
+        <td>
+        <i class="controles material-icons red-text" onclick="quitar('')">cancel</i>
+        </td>
+      </tr>
+      <!--  /row  -->
+      <?php } ?>
 </body>
 </html>
